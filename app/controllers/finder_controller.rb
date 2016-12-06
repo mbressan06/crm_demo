a8ac53c1fbf4ae86a2e169353d94829c2f0ab178
Marcelo Bressan <mbressan06@hotmail.com>
2016-12-05 23:26:44 -0200

    Finished email and alphabetized views

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,5 +1,18 @@
 class FinderController < ApplicationController
   def index
+    @page_title = "All customers"
     @customers = Customer.all
+  end
+
+  def alphabetized
+    @page_title = "Alphabetized customers"
+    @customers = Customer.order(:full_name)
+    render :index
+  end
+
+  def missing_email
+    @page_title = "Missing e-mail customers"
+    @customers = Customer.where(email: '')
+    render :index
   end
 end
